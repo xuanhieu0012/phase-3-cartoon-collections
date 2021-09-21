3b0021b84e5202691da0807819d217c655fa9a7d
Hieu <xuanhieu0012@gmail.com>
2021-09-21 17:19:50 -0500

    commit tree 9b965d23700f3457b8208d9279516362a8f46c9b

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves arr
   # Your code here
+  arr.each_with_index {|dwarve, i| puts "#{i +1}. #{dwarve}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet arr
+  arr.map {|ele| "#{ele.capitalize()}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls arr
+  arr.any? {|word| word.length > 4}
+  
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese arr
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find do |cheese|
+    if cheese_types.include?(cheese)
+      return cheese
+    end
+  end
 end
